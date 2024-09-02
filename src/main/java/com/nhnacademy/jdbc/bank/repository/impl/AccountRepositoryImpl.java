30144a9f7da911352e7f08d71e3b014acec3f917
이한빈 <dlgksqls7218@naver.com>
2024-09-02 09:24:14 +0900

    transaction-bank-account

---
+++ src/main/java/com/nhnacademy/jdbc/bank/repository/impl/AccountRepositoryImpl.java
@@ -0,0 +1,134 @@
+package com.nhnacademy.jdbc.bank.repository.impl;
+
+import com.nhnacademy.jdbc.bank.domain.Account;
+import com.nhnacademy.jdbc.bank.exception.AccountAreadyExistException;
+import com.nhnacademy.jdbc.bank.exception.AccountNotFoundException;
+import com.nhnacademy.jdbc.bank.repository.AccountRepository;
+
+import java.sql.*;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+
+public class AccountRepositoryImpl implements AccountRepository {
+
+    public Optional<Account> findByAccountNumber(Connection connection, long accountNumber){
+        //todo#1 계좌-조회
+        StringBuilder queryBuilder = new StringBuilder();
+        queryBuilder.append("SELECT * FROM jdbc_account WHERE account_number=?;");
+
+        String query = queryBuilder.toString();
+
+        try (PreparedStatement statement = connection.prepareStatement(query);
+        ){
+            statement.setLong(1, accountNumber);
+            ResultSet resultSet = statement.executeQuery();
+
+            if(resultSet.next()){
+                Account account = new Account(
+                        resultSet.getLong("account_number"),
+                        resultSet.getString("name"),
+                        resultSet.getLong("balance")
+                );
+                return Optional.of(account);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public int save(Connection connection, Account account) {
+        //todo#2 계좌-등록, executeUpdate() 결과를 반환 합니다.
+        String query = "INSERT INTO jdbc_account (account_number, name, balance) VALUES (?, ?, ?);";
+
+        try(PreparedStatement statement = connection.prepareStatement(query);){
+
+            statement.setLong(1, account.getAccountNumber());
+            statement.setString(2, account.getName());
+            statement.setLong(3, account.getBalance());
+
+
+            int row = statement.executeUpdate();
+            return row;
+
+        }catch (SQLIntegrityConstraintViolationException  e){
+            return -1;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int countByAccountNumber(Connection connection, long accountNumber){
+        int count=0;
+        //todo#3 select count(*)를 이용해서 계좌의 개수를 count해서 반환
+        String query = "SELECT COUNT(*) FROM jdbc_account WHERE account_number = ?;";
+
+        try(PreparedStatement statement = connection.prepareStatement(query);){
+
+            statement.setLong(1, accountNumber);
+            ResultSet resultSet = statement.executeQuery();
+            if(resultSet.next()) {
+                count = resultSet.getInt(1);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return count;
+    }
+
+    @Override
+    public int deposit(Connection connection, long accountNumber, long amount){
+        //todo#4 입금, executeUpdate() 결과를 반환 합니다.
+        String query = "UPDATE jdbc_account SET balance = balance + ? WHERE account_number = ?; ";
+
+        try(PreparedStatement statement = connection.prepareStatement(query);){
+
+            statement.setLong(1, amount);
+            statement.setLong(2, accountNumber);
+
+            int row = statement.executeUpdate();
+            return row;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int withdraw(Connection connection, long accountNumber, long amount){
+        //todo#5 출금, executeUpdate() 결과를 반환 합니다.
+        String query = "UPDATE jdbc_account SET balance = balance - ? WHERE account_number = ?; ";
+
+        try(PreparedStatement statement = connection.prepareStatement(query);){
+
+            statement.setLong(1, amount);
+            statement.setLong(2, accountNumber);
+
+            int row = statement.executeUpdate();
+            return row;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int deleteByAccountNumber(Connection connection, long accountNumber) {
+        //todo#6 계좌 삭제, executeUpdate() 결과를 반환 합니다.
+        String query = "DELETE FROM jdbc_account WHERE account_number =?; ";
+
+        try(PreparedStatement statement = connection.prepareStatement(query);){
+
+            statement.setLong(1, accountNumber);
+
+            int row = statement.executeUpdate();
+            return row;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }    }
+}
